b8b97fadea04574574b484021ca293b84f4021ea
tyeckh <117521882+tyeckh@users.noreply.github.com>
2026-04-17 11:56:57 +0800

    remove eager importing

--- modules/__init__.py
+++
@@ -1,9 +0,0 @@
-"""GUI modules for the SQL Query Plan Annotator."""
-
-from modules.constants import HIGHLIGHT_COLORS, NODE_COLORS, EXAMPLE_QUERIES
-from modules.themes import ThemeManager
-from modules.syntax import SqlSyntaxHighlighter
-from modules.qep_diagram import QepNodeItem, QepGraphicsView, NODE_W, NODE_H, H_GAP, V_GAP
-from modules.chat_panel import ChatPanel
-from modules.settings_panel import SettingsPanel
-from modules.export import export_results
